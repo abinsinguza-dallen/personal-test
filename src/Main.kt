8317e189c4a925a8b27a0c2d5102bcebb0453d32
-abinsinguza-dallen <dallenabinsinguza@gmail.com>
2022-02-28 14:58:13 +0300

    add file

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+fun main (){
+    var age =50
+    age++
+    println(age)
+    var s =del("speria","brezia")
+}
+fun del(last : String, list: String):String{
+    var del = "$last $list"
+    println(del)
+    return del
+
+}
+
