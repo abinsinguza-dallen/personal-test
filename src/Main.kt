0ddc58770f9a45e8b8f92d584f7de4f9d0abf54d
-abinsinguza-dallen <dallenabinsinguza@gmail.com>
2022-03-03 23:00:54 +0300

    first commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,37 @@
 fun main (){
-    var age =50
-    age++
-    println(age)
-    var s =del("speria","brezia")
+    var text = "akirachix"
+    println(text[0].toString()+text[2]+text[3])
+     var m =sentence("dallen",20)
+    println(m)
+     var d = word()
+    println(d)
+    identity("DALLEN")
+    identity("SPERIA")
 }
-fun del(last : String, list: String):String{
-    var del = "$last $list"
-    println(del)
-    return del
 
-}
+ fun sentence(name: String , years: Int) :String{
+    var f =("Hi, my is $name and i am $years years old")
+     return(f)
+
+ }
+  fun word():Int{
+      var word ="success"
+     var l = word.length
+       return l
+  }
+
+   fun identity(name:String){
+       if(name=="DALLEN")
+           println("thats me")
+       else
+           println("i dont know who that is")
+   }
+
+
+
+
+
+
+
+
 
